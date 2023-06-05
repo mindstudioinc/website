08055162476ebb9f8937459269370be1ebe90dfd
Aleph Diallo <alephdiallo@gmail.com>
2023-06-05 13:01:53 -0400

    Create easter_eggs.js

---
+++ easter_eggs.js
@@ -0,0 +1,18 @@
+console.log(`%c
+_______________________________
+_________/_______\_/____\______
+____________/___\______________
+___________/_____\____/_\______
+ /\/\ ||  |\||  | \ \\
+/    \||  ||\|  |_/ //, INCORPORATED
+" 
+`, `font-family: monospace`);
+console.log(`%c
+(c) 2023 Minds, Inc. All rights reserved.
+" 
+`, `font-family: monospace`);
+
+console.log(`%c
+Congrats! You found a secret!
+" 
+`, `font-family: monospace`);
